0114204e4f179377d3c4d666e4987f222d04dcfe
Pablo <202311460@alu.comillas.edu>
2025-03-26 16:38:40 +0100

    Add configuration and visualization menu scripts

---
+++ src/configuracion.py
@@ -0,0 +1,18 @@
+# configuracion.py
+
+MYSQL_HOST = "localhost"
+MYSQL_PORT = 3306
+MYSQL_USER = "root"
+MYSQL_PASS = "5QyePb9T"
+MYSQL_DB   = "ProyectoAmazon"
+
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+MONGO_DB   = "ProyectoBBDD"
+MONGO_COLLECTION = "ReviewsText"
+
+# Rutas de los ficheros JSON
+TOYS_FILE       = "data/Toys_and_Games_5.json"
+VIDEOGAMES_FILE = "data/Video_Games_5.json"
+MUSIC_FILE      = "data/Digital_Music_5.json"
+INSTRUMENTS_FILE= "data/Musical_Instruments_5.json"
